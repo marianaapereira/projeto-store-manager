c7f6bacd288d16e0dd53189297a439416e79e48b
Mariana Pereira <marianapereira.s@live.com>
2023-09-15 23:21:02 -0300

    Testes da rota /products utilizando a biblioteca http

--- backend/tests/unit/routes/products.router.test.js
+++ backend/tests/unit/routes/products.router.test.js
@@ -1,30 +1,52 @@
-// const chai = require('chai');
+const chai = require('chai');
+const http = require('http');
 
-// const { expect } = chai;
+const { expect } = chai;
 
-const request = require('supertest');
+const app = require('../../../src/app');
 
-// Importe o aplicativo ou módulo do servidor que você deseja testar
-const app = require('../../../src/app'); // Substitua pelo caminho correto para o seu aplicativo
-
-describe('As funções do router', function () {
+describe('A rota /products', function () {
   it('deve retornar status 200 ao fazer uma requisição GET', function (done) {
-    request(app)
-      .get('/products')
-      .expect(200)
-      .end(function (err, _res) {
-        if (err) return done(err);
+    const server = http.createServer(app);
+
+    server.listen(0, function () {
+      const { port } = server.address();
+      const requestOptions = {
+        hostname: 'localhost',
+        port,
+        path: '/products',
+        method: 'GET',
+      };
+
+      const req = http.request(requestOptions, function (res) {
+        expect(res.statusCode).to.equal(200);
+        server.close();
         done();
       });
+
+      req.end();
+    });
   });
 
-  it('deve retornar status 404 ao fazer uma requisição para uma rota inexistente', function (done) {
-    request(app)
-      .get('/products/10')
-      .expect(404)
-      .end(function (err, _res) {
-        if (err) return done(err);
+  it('deve retornar status 404 ao fazer uma requisição GET para uma rota inexistente', function (done) {
+    const server = http.createServer(app);
+
+    server.listen(0, function () {
+      const { port } = server.address();
+      const requestOptions = {
+        hostname: 'localhost',
+        port,
+        path: '/products/10',
+        method: 'GET',
+      };
+
+      const req = http.request(requestOptions, function (res) {
+        expect(res.statusCode).to.equal(404);
+        server.close();
         done();
       });
+
+      req.end();
+    });
   });
 });
